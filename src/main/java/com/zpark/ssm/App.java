b87c94bac18507073d47b669c2a2826dadf8a6f1
gyc <1583079206@qq.com>
2023-05-06 09:41:19 +0800

    hotfix first modify

--- src/main/java/com/zpark/ssm/App.java
+++ src/main/java/com/zpark/ssm/App.java
@@ -3,10 +3,13 @@ package com.zpark.ssm;
 public class App {
 
     public static void main(String[] args) {
+
         System.out.println("Hello Git!");
         System.out.println("Hello Git! gyc master second commit!");
         System.out.println("Hello Git! gyc hotfix first commit!");
         System.out.println("Hello Git! gyc hotfix second commit!");
+
+        System.out.println("Hello Git! gyc hotfix first modify!");
 
     }
 
