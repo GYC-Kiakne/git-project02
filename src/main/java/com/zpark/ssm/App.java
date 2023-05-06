f354a5888b7ca020ff0b615db6f9cc55eb6d3562
gyc <1583079206@qq.com>
2023-05-06 09:10:33 +0800

    first

--- src/main/java/com/zpark/ssm/App.java
+++ src/main/java/com/zpark/ssm/App.java
@@ -4,6 +4,8 @@ public class App {
 
     public static void main(String[] args) {
         System.out.println("Hello Git!");
+        System.out.println("Hello Git! gyc master second commit!");
+        System.out.println("Hello Git! gyc hotfix first commit!");
 
     }
 
